1fbbefba7f3e15a280d63b3a6c8433fefff9cc46
ifnotynot <72943154+ifnotynot@users.noreply.github.com>
2020-11-22 12:11:20 +0100

    Add files via upload

---
+++ lessons.js
@@ -0,0 +1,94 @@
+let lessons = [
+    {
+        "id": 01,
+        "subject": 'Mathematics',
+        "location": "Mauritius",
+        "price": 300,
+        "image": "img/Mathematics.jpg",
+        "availableInventory": 5,
+        "rating": 3,
+    },
+    {
+        "id": 02,
+        "subject": 'Physical Education (P.E.)',
+        "location": "Dubai",
+        "price": 250,
+        "image": "img/Physical Education (P.E.).jpg",
+        "availableInventory":5,
+        "rating": 4
+    },
+    {
+        "id": 03,
+        "subject": 'Art',
+        "location": "Hendon",
+        "price": 200,
+        "image": "img/Art.jpg",
+        "availableInventory":5,
+        "rating": 5
+    },
+    {
+        "id": 04,
+        "subject": 'Music',
+        "location": "Mauritius",
+        "price": 150,
+        "image": "img/Music.jpg",
+        "availableInventory": 5,
+        "rating": 3,
+    },
+    {
+        "id": 05,
+        "subject": 'Geography',
+        "location": "Dubai",
+        "price": 200,
+        "image": "img/Geography.jpg",
+        "availableInventory":5,
+        "rating": 4
+    },
+    {
+        "id": 06,
+        "subject": 'Zoology',
+        "location": "Hendon",
+        "price": 350,
+        "image": "img/Zoology.jpg",
+        "availableInventory":5,
+        "rating": 5
+    },
+    {
+        "id": 07,
+        "subject": 'FOREIGN LANGUAGE',
+        "location": "Mauritius",
+        "price": 170,
+        "image": "img/FOREIGN LANGUAGE.jpg",
+        "availableInventory": 5,
+        "rating": 3,
+    },
+    {
+        "id": 08,
+        "subject": 'Computer',
+        "location": "Dubai",
+        "price": 240,
+        "image": "img/Computer.png",
+        "availableInventory":5,
+        "rating": 4
+    },
+    {
+        "id": 09,
+        "subject": 'HEALTH',
+        "location": "Hendon",
+        "price": 190,
+        "image": "img/HEALTH.jpg",
+        "availableInventory":5,
+        "rating": 5
+    },
+    {
+        "id": 010,
+        "subject": 'Physical Science',
+        "location": "Mauritius",
+        "price": 200,
+        "image": "img/Physical Science.jpg",
+        "availableInventory": 5,
+        "rating": 3,
+    },
+    
+   
+]
